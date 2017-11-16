cadf8b4b23a18f59ad324ffebc0b0a9ff0ff8583
Mikhael Lerman <michael_lerman@yahoo.com>
2017-11-15 17:29:52 -0800

    commit for eXtplorer project from LAPTOP-7KQRMTC0

---
+++ Engineering/ENVIRONMENT/PHP_SERVER/eXtplorer/config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('root','$2a$08$4T.99r.T4ktspPQQIiR8cOdkLH8yxGU5jLfTD86B6h3WdPozFus0u','C:/UniServer/www/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/eXtplorer/','http://localhost/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/eXtplorer/','1','','7',1),
+); 
+?>
