3f16aff13b4095960bfd9fd14a8e7333f95bfc1b
Mikhael Lerman <michael_lerman@yahoo.com>
2017-11-16 09:10:27 -0800

    commit for xfer project from LAPTOP-7KQRMTC0

--- xfer/common/filechange.php
+++
@@ -1,9 +0,0 @@
-<?php
-$target=$_GET['target'];
-$ret=1;
-if (file_exists($target)) $ret=filemtime($target);
-echo $ret;
-//file_put_contents("debug.txt",$target);
-//file_put_contents("debug.txt",$ret . " - " . $target);
-
-?>
