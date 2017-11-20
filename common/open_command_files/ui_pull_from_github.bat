aa29b585b6d7856fdfdb49b0cfaec18d7755ed9c
Mikhael Lerman <michael_lerman@yahoo.com>
2017-11-19 19:00:27 -0800

    commit for common project from WIN7-PC

--- common/open_command_files/ui_pull_from_github.bat
+++ common/open_command_files/ui_pull_from_github.bat
@@ -38,32 +38,22 @@ if "%COMPUTERNAME%" == "XSJMIKHAELL30" ( echo this is XSJMIKHAELL30
 git config --global user.email "michael_lerman@yahoo.com"
 git config --global user.name "Mikhael Lerman"
 git config --global core.safecrlf false
-<<<<<<< HEAD
 git config --global core.sparsecheckout true
-=======
->>>>>>> 676f16f6710e4bfd5aea496140d2993e716b76c0
 
 rem remove previous add
 rem ceci efface les fichier dans le repo github
 rem git rm -r --cached c:\UniServer\www\doc\files\ >nul
-<<<<<<< HEAD
-=======
-rem ceci aussi
->>>>>>> 676f16f6710e4bfd5aea496140d2993e716b76c0
 rem git reset --hard origin/master
 
 
 rem add only this project and subdir
 git add -A %THISPLACEBACKSLASH%\  2>&1
-git remote set-url origin https://mlerman@github.com/mlerman/%REPONAME%.git  2>&1
+call c:\UniServer\www\local\set_git_usep.bat
+git remote set-url origin https://%GITUSEP%@github.com/mlerman/%REPONAME%.git  2>&1
 rem git status
 rem git commit -m "commit for %currentfolder% project from %COMPUTERNAME%"
 rem echo username hint: ati, password hint: nrl14
-<<<<<<< HEAD
 git pull origin master 2>&1
-=======
-git pull origin master --allow-unrelated-histories 2>&1
->>>>>>> 676f16f6710e4bfd5aea496140d2993e716b76c0
 
 
 rem returning to the directory
@@ -71,8 +61,4 @@ rem returning to the directory
 cd %THISDIR%
 
 :end
-<<<<<<< HEAD
 rem pause
-=======
-rem pause
->>>>>>> 676f16f6710e4bfd5aea496140d2993e716b76c0
