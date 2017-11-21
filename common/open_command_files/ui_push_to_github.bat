3e8e254ad4150d37b1e79ccb819282e9a1f8fa44
Mikhael Lerman <michael_lerman@yahoo.com>
2017-11-21 15:24:06 -0800

    commit for common project from LAPTOP-7KQRMTC0

--- common/open_command_files/ui_push_to_github.bat
+++ common/open_command_files/ui_push_to_github.bat
@@ -51,7 +51,7 @@ git update-index --no-assume-unchanged %THISPLACEBACKSLASH%\*
 
 rem add only this project and subdir
 git add -A %THISPLACEBACKSLASH%\ 2>&1
-call c:\UniServer\www\local\set_git_usep.bat
+call c:\UniServer\www\local\set_git_usep.bat 2>&1
 git remote set-url origin https://%GITUSEP%@github.com/mlerman/%REPONAME%.git 2>&1
 rem git status
 rem commit only this directory
