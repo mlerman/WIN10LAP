ff1ddb8e13aa65ea48cf11a9933ba97c60fc3035
Mikhael Lerman <michael_lerman@yahoo.com>
2017-11-28 17:33:33 -0800

    commit for common project from LAPTOP-7KQRMTC0

---
+++ common/open_command_files/ui_pull_from_gdrive.bat
@@ -0,0 +1,23 @@
+@echo off
+set REPONAME=WIN10LAP
+set THISDIR=%CD%
+
+set THISPLACE=%cd%
+set THISPLACE=%THISPLACE:~27,512%
+set THISPLACESLASH=%THISPLACE%
+set THISPLACEBACKSLASH=%THISPLACE%
+set THISPLACE=%THISPLACE:\=-%
+
+set THISPLACESLASH=%THISPLACESLASH:\=/%
+echo THISPLACE=%THISPLACE%
+echo THISPLACESLASH=%THISPLACESLASH%
+echo THISPLACEBACKSLASH=%THISPLACEBACKSLASH%
+echo REPONAME=%REPONAME%
+for %%a in (.) do set currentfolder=%%~na
+echo current directory name: %currentfolder%
+
+c:\UniServer\www\doc\files\ThisPC\install_rclone\rclone.exe copy remote:%THISPLACESLASH% .  -v 2>&1
+
+
+:end
+rem pause
