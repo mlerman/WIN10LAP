c3713eb9f41b398409eae8c82107de9a17bae77c
Mikhael Lerman <michael_lerman@yahoo.com>
2017-12-12 12:07:23 -0800

    commit for memory_test project from XSJMIKHAELL30

--- Engineering/ENVIRONMENT/XSCT/memory_test/init.tcl
+++ Engineering/ENVIRONMENT/XSCT/memory_test/init.tcl
@@ -1,5 +1,6 @@
-puts "Running init.tcl"
+puts "Running init.tcl\nConnecting to HW_SERVER on $env(HW_SERVER_HOST)"
 connect -host $env(HW_SERVER_HOST) -port 3121
+puts "Setting target Cortex-A53*#0"
 targets -set -nocase -filter {name =~ "Cortex-A53*#0"}
 
 
