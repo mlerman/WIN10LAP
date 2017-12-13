73d9a0c53a8c7db104b40139850c4972f73ff73f
Mikhael Lerman <michael_lerman@yahoo.com>
2017-12-13 10:32:19 -0800

    commit for common project from LAPTOP-7KQRMTC0

--- common/env.php
+++ common/env.php
@@ -11,7 +11,7 @@ $targetdir="";
   if (isset($_POST['submit'])) {
     if(isset($_POST['envVar']) && !empty($_POST['envVar'])) {
       $envVar = $_POST['envVar'];
-	  $txt = "<a href=\"".$_SERVER["PHP_SELF"]."?targetdir=".$targetdir."&delaction=1&name=".$envVar."\"><img src=\"/doc/images/delete.png\"></a>&nbsp;<label>".$envVar."</label>  <span id=\"".$targetdir.'/'.$envVar."\" class=\"editText\"></span><hr/>";
+	  $txt = "<a href=\"".$_SERVER["PHP_SELF"]."?targetdir=".$targetdir."&disaction=1&name=".$envVar."\"><img src=\"/doc/images/on.png\"></a>&nbsp;<a href=\"".$_SERVER["PHP_SELF"]."?targetdir=".$targetdir."&delaction=1&name=".$envVar."\"><img src=\"/doc/images/delete.png\"></a>&nbsp;<label>".$envVar."</label>  <span id=\"".$targetdir.'/'.$envVar."\" class=\"editText\"></span><hr/>";
       file_put_contents($targetdir.'/entries.html', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
       file_put_contents($targetdir.'/'.$envVar.'.sh.bat', "set ".$envVar."=new" , FILE_APPEND | LOCK_EX);
     }
@@ -39,6 +39,62 @@ $targetdir="";
 
     unlink($targetdir.'/'.$_GET["name"].".sh.bat");
   }
+
+  if (isset($_GET['disaction'])) {
+    $file = $targetdir.'/entries.html'; 
+    $fh = fopen($file, 'r') or die('Could not open file: '.$file); 
+    $i = 0; 
+    $content="";
+    while (!feof($fh)) { 
+       $buffer = fgets($fh); 
+   
+       if (strpos($buffer, "<label>".$_GET["name"]."</label>") !== false) {
+         // TODO
+		$buffer=str_replace("disaction","enaction",$buffer);
+		$buffer=str_replace("on.png","off.png",$buffer);
+        $content.=$buffer;
+       }  else {
+         //echo $buffer."<br/>\n";
+        $content.=$buffer;
+      }   
+      $i++;   
+    } 
+    fclose($fh); 
+
+    file_put_contents($file, $content);
+
+	copy($targetdir.'/'.$_GET["name"].".sh.bat", $targetdir.'/'.$_GET["name"].".sh.bat.0");
+    unlink($targetdir.'/'.$_GET["name"].".sh.bat");
+  }
+  
+
+  if (isset($_GET['enaction'])) {
+    $file = $targetdir.'/entries.html'; 
+    $fh = fopen($file, 'r') or die('Could not open file: '.$file); 
+    $i = 0; 
+    $content="";
+    while (!feof($fh)) { 
+       $buffer = fgets($fh); 
+   
+       if (strpos($buffer, "<label>".$_GET["name"]."</label>") !== false) {
+         // TODO
+		$buffer=str_replace("enaction","disaction",$buffer);
+		$buffer=str_replace("off.png","on.png",$buffer);
+        $content.=$buffer;
+       }  else {
+         //echo $buffer."<br/>\n";
+        $content.=$buffer;
+      }   
+      $i++;   
+    } 
+    fclose($fh); 
+
+    file_put_contents($file, $content);
+
+	copy($targetdir.'/'.$_GET["name"].".sh.bat.0", $targetdir.'/'.$_GET["name"].".sh.bat");
+    unlink($targetdir.'/'.$_GET["name"].".sh.bat.0");
+  }
+  
 ?>
 
 
@@ -125,7 +181,11 @@ New environment variable name: <input type="text" name="envVar" placeholder="Var
 	  $var_name=$elem_label->nodeValue;
       //echo "item ".$item_num." ".$span->nodeValue." ".$var_name."<br/>\n";
 	  $elem = $doc->getElementsByTagName('span')->item($item_num);
-	  $strVal=file_get_contents($targetdir.'/'.$var_name.".sh.bat");
+	  if (file_exists($targetdir.'/'.$var_name.".sh.bat")) {
+		$strVal=file_get_contents($targetdir.'/'.$var_name.".sh.bat");
+	  }	else {
+		$strVal=file_get_contents($targetdir.'/'.$var_name.".sh.bat.0");
+	  }
 	  $elem->nodeValue=$strVal;
 	  $item_num++;
   }  
