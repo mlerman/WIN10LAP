22ed67edfcfc5090a2e7e1cbbc7350534339a198
Mikhael Lerman <michael_lerman@yahoo.com>
2018-01-02 18:21:38 -0800

    commit for memory_test project from LAPTOP-7KQRMTC0

--- Engineering/ENVIRONMENT/XSCT/memory_test/init.tcl
+++ Engineering/ENVIRONMENT/XSCT/memory_test/init.tcl
@@ -1,7 +1,7 @@
 puts "Running init.tcl\nConnecting to HW_SERVER on $env(HW_SERVER_HOST)"
 connect -host $env(HW_SERVER_HOST) -port 3121
 puts "Setting target Cortex-A53*#0"
-targets -set -nocase -filter {name =~ "Cortex-A53*#0"}
+targets -set -nocase -filter {name =~ "$env(TARGET_SET_FILTER)"}
 
 
 
--- Engineering/ENVIRONMENT/XSCT/memory_test/system_debugger_using_debug_hello_minimum.elf_on_local.tcl
+++ Engineering/ENVIRONMENT/XSCT/memory_test/system_debugger_using_debug_hello_minimum.elf_on_local.tcl
@@ -5,12 +5,16 @@ puts "zynqmp_utils..."
 source c:/Xilinx/SDK/2017.2/scripts/sdk/util/zynqmp_utils.tcl
 puts "target..."
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2NC 210308A12403"} -index 1
+#targets -set -nocase -filter {name =~ "$env(TARGET_SET_FILTER)"}
+
 puts "loadhw system.hdf..."
 loadhw -hw C:/UniServer/www/doc/files/Engineering/ENVIRONMENT/VIVADO/ZU3/project_1.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x80000000 0xbfffffff} {0x400000000 0x5ffffffff} {0x1000000000 0x7fffffffff}]
 puts "configparams..."
 configparams force-mem-access 1
 puts "target..."
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2NC 210308A12403"} -index 1
+#targets -set -nocase -filter {name =~ "$env(TARGET_SET_FILTER)"}
+
 puts "psu_init..."
 #source C:/UniServer/www/doc/files/Engineering/ENVIRONMENT/VIVADO/ZU3/project_1.sdk/design_1_wrapper_hw_platform_0/psu_init.tcl
 # one line blocks, it's commented
@@ -24,7 +28,9 @@ puts "psu_ps_pl_reset_config..."
 psu_ps_pl_reset_config
 catch {psu_protection}
 puts "target..."
-targets -set -nocase -filter {name =~"*A53*0" && jtag_cable_name =~ "Digilent JTAG-SMT2NC 210308A12403"} -index 1
+#targets -set -nocase -filter {name =~"*A53*0" && jtag_cable_name =~ "Digilent JTAG-SMT2NC 210308A12403"} -index 1
+targets -set -nocase -filter {name =~ "$env(TARGET_SET_FILTER)"}
+
 puts "rst -processor..."
 rst -processor
 puts "dow hello_minimum.elf..."
