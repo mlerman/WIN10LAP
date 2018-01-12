0e29425480e6cdad4ee0cdbd7cb5ffd9f6e83cba
Mikhael Lerman <michael_lerman@yahoo.com>
2018-01-12 12:06:17 -0800

    commit for common project from XSJMIKHAELL30

--- common/downloadfile.php
+++ common/downloadfile.php
@@ -235,7 +235,10 @@ if(($CurrOS=='Linux')||($CurrOS=='Android')) {
     $text.=$str_mount;
 	// now create a symbolic link ex ln -s /home/user/xsjmikhaell30/files /home/user/files
 	// f overwrite existing link
+	// des fois il semble que cette commande ne marche pas dans le script
 	$text.="  sudo ln -sfn /home/user/".$host."/files /home/user/files\n";
+	// now test the link and eventually pause
+	test -L /home/user/files && echo "symbolic link created successfully" || echo "could not create symbolic link /home/user/files" && sed -n q </dev/tty
 	
 	// pause for debug
     //$text.="  read -p \"Press [Enter] key to continue... \" "."\n";
