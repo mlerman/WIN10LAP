e4b6200556a8279f7616c195376a0b634ca1ff44
Mikhael Lerman <michael_lerman@yahoo.com>
2018-01-24 08:29:36 -0800

    commit for common project from WIN7-PC

--- common/addedit.php
+++ common/addedit.php
@@ -29,9 +29,10 @@ echo "fname=".$fname."<br/>\n";
 
 $drive=$path[0];
 
-$link="http://".$_SERVER["HTTP_HOST"]."/doc/files/common/downloadfile.php?fname=".$fname."&targetdir=".$targetdir."&targetfile=".$targetfile."&perma=C:\UniServer\www\doc\files\common\perma&drive=".$drive.":";
+//$link="http://".$_SERVER["HTTP_HOST"]."/doc/files/common/downloadfile.php?fname=".$fname."&targetdir=".$targetdir."&targetfile=".$targetfile."&perma=C:\UniServer\www\doc\files\common\perma&drive=".$drive.":";
+$link="/doc/files/common/downloadfile.php?fname=".$fname."&targetdir=".$targetdir."&targetfile=".$targetfile.'&perma=C:\UniServer\www\doc\files\common\perma&drive='.$drive.":";
 
-$str= "<a href='".$link."'>".$path."</a>&nbsp;<a href=\"\" onclick='openOnce(\"/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/ICEcoder/\", \"editor\", \"".$targetdirdoc.$targetfile."\"); return false;'><img src=\"/doc/images/text.png\"/></a>&nbsp;<a href='/viewfile/view.php?fname=".$targetfile."&targetdir=".$targetdir."'><img src='/doc/images/view.jpg'/></a>&nbsp;<a href='/doc/files/common/downloadfile.php?fname=ui_total_commander.run&urldir=".$parent_dir."&targetdir=".$targetdir.$targetfile."&perma=C:\UniServer\www\doc\files\common\perma' ><img src='/doc/images/totalcommander16.png' /></a><br/>\n";
+$str= "<a href='".$link."'>".$path."</a>&nbsp;<a href=\"\" onclick='openOnce(\"/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/ICEcoder/\", \"editor\", \"".$targetdirdoc.$targetfile."\"); return false;'><img src=\"/doc/images/text.png\"/></a>&nbsp;<a href='/viewfile/view.php?fname=".$targetfile."&targetdir=".$targetdir."'><img src='/doc/images/view.jpg'/></a>&nbsp;<a href='/doc/files/common/downloadfile.php?fname=ui_total_commander.run&urldir=".$parent_dir."&targetdir=".$targetdir.$targetfile."&perma=C:\UniServer\www\doc\'.'files\common\perma' ><img src='/doc/images/totalcommander16.png' /></a><br/>\n";
 echo "str=".$str."<br/>\n";
 file_put_contents($recentedit, $str, FILE_APPEND);
 
