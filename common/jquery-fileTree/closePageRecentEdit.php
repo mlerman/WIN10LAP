e91d878b5752e89707badaf211b87e729e85dc16
Mikhael Lerman <michael_lerman@yahoo.com>
2018-01-23 20:08:19 -0800

    commit for common project from WIN7-PC

--- common/jquery-fileTree/closePageRecentEdit.php
+++ common/jquery-fileTree/closePageRecentEdit.php
@@ -22,7 +22,9 @@ echo ".recentedit path=".$recentedit."<br/>\n";
 
 $fname="ui_edit_this.run";
 echo "fname=".$fname."<br/>\n";
-$link="http://".$_SERVER["HTTP_HOST"]."/doc/files/common/downloadfile.php?fname=".$fname."&targetdir=".$targetdir."&targetfile=".$targetfile."&perma=C:\UniServer\www\common\perma&drive=".$_GET['drive'].":";
+//$link="http://".$_SERVER["HTTP_HOST"]."/doc/files/common/downloadfile.php?fname=".$fname."&targetdir=".$targetdir."&targetfile=".$targetfile."&perma=C:\UniServer\www\common\perma&drive=".$_GET['drive'].":";
+// enlever le host pour que ca marche sur tous les pc
+$link="/doc/files/common/downloadfile.php?fname=".$fname."&targetdir=".$targetdir."&targetfile=".$targetfile."&perma=C:\UniServer\www\common\perma&drive=".$_GET['drive'].":";
 $str= "<a href='".$link."'>".$fullfname."</a>&nbsp;<a href=\"\" onclick='openOnce(\"/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/ICEcoder/\", \"editor\", \"".$targetdirdoc.$targetfile."\"); return false;'><img src=\"/doc/images/text.png\"/></a>&nbsp;<a href='/viewfile/view.php?fname=".$targetfile."&targetdir=".$targetdir."'><img src='/doc/images/view.jpg'/></a><br/>\n";
 //echo "<a href='".$link."'>".$fullfname."</a>";
 
