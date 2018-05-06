5c0c21c98784ada1ee6761ac82f00ce889fd6a44
Mikhael Lerman <michael_lerman@yahoo.com>
2018-05-05 17:51:04 -0700

    commit for common project from CELINE-PC

--- common/addclip.php
+++ common/addclip.php
@@ -4,16 +4,48 @@ $path=$_GET['path'];
 echo "path=".$path."<br/>";
 $parent=$_GET['parent'];
 echo "parent=".$parent."<br/>\n";
+$label=$_GET['label'];
+echo "label=".$label."<br/>\n";
 
 $parent_dir = "/doc/files".$_GET['parent'];
 
 $cliptab="c:/UniServer/www/doc/files".$parent.".cliptab";
 echo ".cliptab path=".$cliptab."<br/>\n";
 
+$strfile = file_get_contents( $cliptab ); // get the contents, and echo it out.
+//echo $strfile;
+
+
+/*
+detect this:
+] 
+    ]
+    
+    
+in 
+
+    [ 
+    ['toto', 'toto'] ,
+    ['titi', 'titi'] ,
+    ['tutu', 'tutu'] ,
+    ['tata', 'tata'] ,
+    ['coucou', 'coucou'] 
+    ]
+
+*/
+if ($label=="") $label=$path;
+if(strstr($strfile, "] \n    ]")) {
+
+  //echo "<br/>found ! <br/>";
+  $strfile=str_replace("] \n    ]", "] ,\n    ['".$path."', '".$label."'] \n    ]", $strfile);
+  //echo $strfile;
+  file_put_contents($cliptab, $strfile);
+}
+
 ?>
 
 <script>
 //this does not refresh but come back to the same vertical scroll position
-//sessionStorage.setItem("forcerefresh", "yes");
-//window.history.back();
+sessionStorage.setItem("forcerefresh", "yes");
+window.history.back();
 </script>
