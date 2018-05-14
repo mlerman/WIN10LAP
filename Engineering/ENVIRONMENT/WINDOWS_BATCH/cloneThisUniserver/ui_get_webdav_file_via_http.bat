43042fae2e4169278eee5568166be15067469d55
Mikhael Lerman <michael_lerman@yahoo.com>
2018-05-13 18:55:30 -0700

    commit for cloneThisUniserver project from CELINE-PC

--- Engineering/ENVIRONMENT/WINDOWS_BATCH/cloneThisUniserver/ui_get_webdav_file_via_http.bat
+++ Engineering/ENVIRONMENT/WINDOWS_BATCH/cloneThisUniserver/ui_get_webdav_file_via_http.bat
@@ -1,4 +1,4 @@
-start http://mlerman:normerel@siliconkit.com/dav/micronsov/Uniserver/www/doc/files/Engineering/ENVIRONMENT/WINDOWS_BATCH/cloneThisUniserver/uniserver.zip
+start http://mlerman:passwordnrl@siliconkit.com/dav/micronsov/Uniserver/www/doc/files/Engineering/ENVIRONMENT/WINDOWS_BATCH/cloneThisUniserver/uniserver.zip
 if exists c:\Users\mlerman\Downloads\uniserver.zip ( echo found
 dir c:\Users\mlerman\Downloads\uniserver.zip
 )
