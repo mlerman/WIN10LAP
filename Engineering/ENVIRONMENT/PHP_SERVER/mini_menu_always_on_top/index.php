3c1027cc455062ff1cc193da246a649fbc37ea67
Mikhael Lerman <michael_lerman@yahoo.com>
2018-07-18 12:56:39 -0700

    commit for mini_menu_always_on_top project from LAPTOP-7KQRMTC0

---
+++ Engineering/ENVIRONMENT/PHP_SERVER/mini_menu_always_on_top/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>MiniMenu</title>
+</head>
+<body>
+&nbsp;<a href="/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/frequent_copy_paste/" target="chartime" onclick="javascript:void window.open('/doc/files/Engineering/ENVIRONMENT/PHP_SERVER/frequent_copy_paste/','1481656702602','width=800,height=800,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=100,top=50');return false;"><img src="/doc/images/copyclip.png" title="frequent copy paste" /></a>
+<br/>
+</body>
+</html>
