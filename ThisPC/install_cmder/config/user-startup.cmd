78b25c024f3bb00408084f6e749537ff46466ece
Mikhael Lerman <michael_lerman@yahoo.com>
2018-09-29 16:50:20 -0700

    commit for install_cmder project from DESKTOP-MCQS4FT

---
+++ ThisPC/install_cmder/config/user-startup.cmd
@@ -0,0 +1,6 @@
+:: use this file to run your own startup commands 
+:: use @ in front of the command to prevent printing the command
+ 
+:: @call "C:\Program Files (x86)\Git/cmd/start-ssh-agent.cmd
+:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+ 
