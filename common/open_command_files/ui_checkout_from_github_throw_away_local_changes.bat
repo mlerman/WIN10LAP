89510aa10d6201039e3908e645ae651c42f27a9e
Mikhael Lerman <michael_lerman@yahoo.com>
2018-12-23 20:59:45 -0800

    commit for common project from LAPTOP-7KQRMTC0

--- common/open_command_files/ui_checkout_from_github_throw_away_local_changes.bat
+++ common/open_command_files/ui_checkout_from_github_throw_away_local_changes.bat
@@ -49,17 +49,17 @@ git update-index --assume-unchanged c:\UniServer\www\doc\files\*
 git update-index --no-assume-unchanged %THISPLACEBACKSLASH%\*
 
 rem add only this project and subdir
-git add -A %THISPLACEBACKSLASH%\  2>&1
-git remote set-url origin https://mlerman@github.com/mlerman/%REPONAME%.git  2>&1
-git remote update 2>&1
-git fetch origin 2>&1
+git add -A %THISPLACEBACKSLASH%\  	
+git remote set-url origin https://mlerman@github.com/mlerman/%REPONAME%.git  
+git remote update 
+git fetch origin 
 
 rem a commit will delete a file in the repo
 rem git commit -m "before checkout" 2>&1
 
-git reset --hard origin/master 2>&1
+git reset --hard origin/master 
 
-git checkout HEAD %THISPLACEBACKSLASH%\ 2>&1
+git checkout HEAD %THISPLACEBACKSLASH%\ 
 
 rem returning to the directory
 :test
